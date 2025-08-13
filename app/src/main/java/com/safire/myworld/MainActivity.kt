1d33eba96e1e7cc7aa2c96231d2250794801607f
Safire <Safire101100@outlook.com>
2025-08-13 11:40:04 +0430

    - Theme.kt: Removed the dynamics color feature - MainActivity.kt:     * Added top App Bar     * Refactored code and added more functions - Added drawables for background pictures of each continent - Changed the app launcher icon

--- app/src/main/java/com/safire/myworld/MainActivity.kt
+++ app/src/main/java/com/safire/myworld/MainActivity.kt
@@ -4,41 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultShadowColor
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose.MyWorldTheme
+import com.example.compose.onPrimaryContainerDark
 import com.safire.myworld.model.Continent
 import kotlinx.serialization.json.Json
 import java.io.InputStream
@@ -63,24 +75,36 @@ private fun retrieveContinents(file: InputStream): List<Continent> {
     continents.addAll(Json.decodeFromString<List<Continent>>(jsonString))
 
     return continents
-
 }
 
 @Composable
 fun MyWorldApp(continents: List<Continent>, modifier: Modifier = Modifier) {
 
-    val drawables = listOf(R.drawable.yogyakarta)
+    val drawables = listOf(
+        R.drawable.asia_mayon,
+        R.drawable.africa_pyramids,
+        R.drawable.europe__colloseum,
+        R.drawable.north_america_capitol,
+        R.drawable.rio_de_janeiro,
+        R.drawable.melbourne,
+        R.drawable.antaractica_penguins
+    )
 
-    Scaffold(modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(
+        
+        topBar = {
+            MyWorldTopBar()
+                 },
+        ) { innerPadding ->
 
 
         LazyVerticalGrid(
-            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+            contentPadding = innerPadding,
             columns = GridCells.Fixed(2)
         ) {
 
-            items(continents) {
-                ContinentItem(it, modifier = Modifier.padding(innerPadding))
+            itemsIndexed(continents) { index, continent ->
+                ContinentItem(continent, drawables.get(index))
             }
         }
 
@@ -89,11 +113,11 @@ fun MyWorldApp(continents: List<Continent>, modifier: Modifier = Modifier) {
 
 
 @Composable
-fun ContinentItem(continent: Continent, modifier: Modifier = Modifier) {
+fun ContinentItem(continent: Continent, @DrawableRes drawableId: Int, modifier: Modifier = Modifier) {
     Card(
         elevation = CardDefaults.cardElevation(4.dp),
         border = BorderStroke(2.dp, MaterialTheme.colorScheme.onPrimaryContainer),
-        modifier = modifier
+        modifier = Modifier
             .padding(4.dp)
             .size(250.dp)
     ) {
@@ -104,10 +128,11 @@ fun ContinentItem(continent: Continent, modifier: Modifier = Modifier) {
         ) {
 
             Image(
-                painterResource(R.drawable.yogyakarta),
+                painterResource(drawableId),
                 contentScale = ContentScale.FillBounds,
                 contentDescription = null,
-                alpha = 0.3f, // transparency
+                alpha = 0.35f, // transparency
+
             )
 
             Text(
@@ -124,8 +149,6 @@ fun ContinentItem(continent: Continent, modifier: Modifier = Modifier) {
 
             )
 
-
-
             Column(
                 verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
                 modifier = Modifier
@@ -137,25 +160,40 @@ fun ContinentItem(continent: Continent, modifier: Modifier = Modifier) {
                     painterResource(R.drawable.population),
                     "Population",
                     String.format("%,d", continent.population),
-                    color = MaterialTheme.colorScheme.onTertiaryContainer
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
 
                 IconText(
                     painterResource(R.drawable.area_icon),
                     "Area",
                     String.format("%,d km²", continent.area),
-                    color = MaterialTheme.colorScheme.onTertiaryContainer
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
 
                 IconText(
                     painterResource(R.drawable.flag_icon),
                     "Countries",
                     String.format("%d", continent.numberOfCountries.toInt()),
-                    color = MaterialTheme.colorScheme.onTertiaryContainer
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyWorldTopBar(modifier: Modifier = Modifier) {
+    TopAppBar(title = {
+        Text("My World", style = MaterialTheme.typography.titleMedium, fontSize = 25.sp, fontWeight = FontWeight.ExtraBold, letterSpacing = 8.sp)
+    }, colors = TopAppBarDefaults.topAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+    ),
+        navigationIcon = {
+            Image(painterResource(R.drawable.app_icon), contentDescription = "app icon", Modifier.size(32.dp)) }
+    )
+
 }
 
 @Composable
@@ -165,7 +203,7 @@ fun IconText(
     text: String,
     color: Color,
 ) {
-    Row(horizontalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.padding(4.dp)) {
+    Row(horizontalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.padding(8.dp)) {
         Icon(
             icon,
             contentDescription = iconDescription,
@@ -174,8 +212,18 @@ fun IconText(
         Text(
             text,
             color = color,
-            style = MaterialTheme.typography.titleMedium
+            fontWeight = FontWeight.Medium,
+            style = MaterialTheme.typography.titleMedium.copy(
+
+                shadow = Shadow(Color.White, offset = Offset(4f, 5f), blurRadius = 16f),
+
+            ),
+            fontSize = 16.sp,
+
+
+
         )
+
     }
 }
 
