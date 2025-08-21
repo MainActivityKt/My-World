8c49389c7786826e81d311ad927f85de8082b94d
Safire <76739907+MainActivityKt@users.noreply.github.com>
2025-08-22 01:47:17 +0430

    - Added the list of countries in data.json - Changed the Country item in Country.kt data class

--- app/src/main/java/com/safire/myworld/model/Country.kt
+++ app/src/main/java/com/safire/myworld/model/Country.kt
@@ -4,7 +4,18 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Country(
-    val name: String? = null,
-    val capital: String? = null
-    // Add other fields later
+    val name: String,
+    val countryCode: String,
+    val capital: String,
+    val population: Long,
+    val area: Float,
+    val subregion: String,
+    val languages: List<String>,
+    val currency: String,
+    val callingCode: String,
+    val flag: String,
+    val gdp: Long,
+    val shortDescription: String,
+    val description: String
+
 )
